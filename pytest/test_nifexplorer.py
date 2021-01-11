2ce435ef69545dede00bd797a7c93b8a10f0ed1d
Aaron Thompson <Aaron1178@gmail.com>
2021-01-11 13:35:40 +1100

    PyTest Updates. Added in more tests, and started grouping them.

--- pytest/test_nifexplorer.py
+++ pytest/test_nifexplorer.py
@@ -11,86 +11,49 @@ sys.path.append(path)
 from NifExplorer import NifExplorer
 from NifExplorer import NifFormat
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope='session')
 def setup_nifExplorer():
     '''Returns a Nif Explorer Instance'''        
     explorer = NifExplorer()
 
-    explorer.BlockType = NifFormat.NiNode
-    explorer.ResultPath = "String"
-    explorer.SearchPath = None
-        
+    explorer.SetBlockType(NifFormat.NiNode)
+    explorer.SetResultPath("\\pytest\\results")
+    explorer.SetSearchPath("\\pytest\\nif\\base")
+
     return explorer   
 
 @pytest.mark.usefixtures("setup_nifExplorer")
 class TestNifExplorer:
 
-    def BlockType_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.BlockType == None
-
-    def SearchPath_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.SearchPath == None
-
-    def ResultPath_None(self, setup_nifExlorer):
-        assert setup_nifExlorer.ResultPath == None
-        
-    @pytest.mark.parametrize('funcs', [SearchPath_None, ResultPath_None, BlockType_None])
-    def test_NifExplorer_Variables_Equal_None(self, setup_nifExplorer, funcs):
-        funcs(self,setup_nifExplorer)
-
-    def test_Class_Is_None(self, setup_nifExplorer):
-        assert setup_nifExplorer == None
-
-    def test_BlockType_Not_None(self, setup_nifExplorer):
+    def NifExlorer_BlockType_Is_Not_None(self, setup_nifExplorer):
         assert setup_nifExplorer.BlockType != None
 
-    def test_BlockType_Is_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.BlockType == None
-
-    def test_BlockType_Is_String(self, setup_nifExplorer):
-        assert isinstance(setup_nifExplorer.BlockType, str)
-
-    def test_BlockType_Not_String(self, setup_nifExplorer):
-        assert not isinstance(setup_nifExplorer.BlockType, str)   
-        
-    def test_SearchPath_Not_None(self, setup_nifExplorer):
+    def NifExplorer_SearchPath_Is_Not_None(self, setup_nifExplorer):
         assert setup_nifExplorer.SearchPath != None
-    
-    def test_SearchPath_Is_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.SearchPath == None
-    
-    def test_SearchPath_Is_String(self, setup_nifExplorer):
-        assert isinstance(setup_nifExplorer.SearchPath, str)
-  
-    def test_SearchPath_Not_String(self, setup_nifExplorer):
-        assert not isinstance(setup_nifExplorer.SearchPath, str)
 
-    def test_SearchPath_Directory_Does_Exist(self, setup_nifExplorer):
-        assert os.path.exists(setup_nifExplorer.SearchPath) == True
-
-    def test_SearchPath_Directory_Does_Not_Exist(self, setup_nifExplorer):
-        if setup_nifExplorer.SearchPath == None or os.path.exists(setup_nifExplorer.SearchPath) == False:
-            assert True
-
-    def test_ResultPath_Not_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.ResultPath != None
-    
-    def test_ResultPath_Is_None(self, setup_nifExplorer):
-        assert setup_nifExplorer.ResultPath == None
-    
-    def test_ResultPath_Is_String(self, setup_nifExplorer):
-        assert isinstance(setup_nifExplorer.ResultPath, str)
-  
-    def test_ResultPath_Not_String(self, setup_nifExplorer):
-        assert not isinstance(setup_nifExplorer.ResultPath, str)
+    def NifExplorer_ResultPath_Is_Not_None(self, setup_nifExlorer):
+        assert setup_nifExlorer.ResultPath != None
         
-    def test_ResultPath_Directory_Does_Exist(self, setup_nifExplorer):
+    @pytest.mark.parametrize('funcs', [NifExplorer_SearchPath_Is_Not_None, NifExplorer_ResultPath_Is_Not_None, NifExlorer_BlockType_Is_Not_None])
+    def test_NifExplorer_Variables_Equal_Not_None(self, setup_nifExplorer, funcs):
+        funcs(self,setup_nifExplorer)
+        
+    def NifExplorer_ResultPath_Directory_Exists(self, setup_nifExplorer):
+        print("NifExplorer_ResultPath_Directory_Exists[Directory]: %s" % setup_nifExplorer.ResultPath)
         assert os.path.exists(setup_nifExplorer.ResultPath) == True
 
-    def test_ResultPath_Directory_Does_Not_Exist(self, setup_nifExplorer):
-        if setup_nifExplorer.ResultPath == None or os.path.exists(setup_nifExplorer.ResultPath) == False:
-            assert True
+    def NifExplorer_SearchPath_Directory_Exists(self, setup_nifExplorer):
+        assert os.path.exists(setup_nifExplorer.SearchPath) == True
 
+    def NifExplorer_SearchPath_Directory_Contains_No_Forward_Slashes(self, setup_nifExplorer):
+        assert setup_nifExplorer.SearchPath.count('/') < 1
+
+    def NifExplorer_ResultPath_Directory_Contains_No_Forward_Slashes(self, setup_nifExplorer):
+        assert setup_nifExplorer.ResultPath.count('/') < 1
+
+    @pytest.mark.parametrize('funcs', [NifExplorer_ResultPath_Directory_Exists, NifExplorer_SearchPath_Directory_Exists, NifExplorer_SearchPath_Directory_Contains_No_Forward_Slashes, NifExplorer_ResultPath_Directory_Contains_No_Forward_Slashes])
+    def test_NifExplorer_Directories_Exist_And_Paths_Contain_No_Forward_Slashes(self, setup_nifExplorer, funcs):
+        funcs(self, setup_nifExplorer)
 
 if __name__ == "__main__":
     pytest.main()
