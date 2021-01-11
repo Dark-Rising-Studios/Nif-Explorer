507e4da39467d4e60e900bba16332e66cbc85e82
Aaron Thompson <Aaron1178@gmail.com>
2021-01-11 15:56:05 +1100

    NifExplorer - Code Redesign From Scratch based on TDD.

--- nifexplorer.py
+++ nifexplorer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 from pyffi.formats.nif import NifFormat
 
@@ -17,8 +18,16 @@ class NifExplorer():
 
     """Set the BlockType, will resolve down to a string, but a valid class can be passed. e.g: NifFormat.NiNode"""
     def SetBlockType(self, InBlockType):        
+        if InBlockType == None:
+            assert "NifExplorer.SetBlockType(): InBlockType is None!"
+
         if isinstance(InBlockType, str):
-            self.BlockType = self.FindBlockTypeByName(InBlockType)
+            if self.FindBlockTypeByName(InBlockType) == None:
+                assert "NifExplorer.SetBlockType(): Cannot find BlockType! Exiting!"
+                sys.exit()
+
+            else:
+                self.BlockType = self.FindBlockTypeByName(InBlockType)
 
         else:
             BlockTypeToString = str(InBlockType)
@@ -38,8 +47,16 @@ class NifExplorer():
         elif self.MakeAbsolutePath(__file__, InSearchPath) != None:
             self.SearchPath = self.MakeAbsolutePath(__file__, InSearchPath)
 
+            if not os.path.exists(self.SearchPath):
+                assert "NifExplorer.SetSearchPath(): Search Path does not exist!"
+                sys.exit()
+
+            if not self.DirectoryContainsNifRecursively(self.SearchPath):
+                assert "No .nif files found recursively!"
+                sys.exit()
         else:
             assert "NifExplorer.SetSearchPath(): Cannot Resolve Search Path!"
+            sys.exit()
 
     """Set the Result Path, if the specified path doesn't exist, it will create it"""
     def SetResultPath(self, InResultPath):
@@ -59,10 +76,52 @@ class NifExplorer():
 
         else:
             assert "NifExplorer.SetResultPath(): Cannot Resolve Result Path!"
+   
+    """Start and return a timer"""
+    def StartTimer():
+        return time.time()
+
+    """Stops the timer and Returns the End Time"""
+    def EndTimer():
+        return time.time()
+
+    """Get Time Elapsed time"""
+    def GetTimeElapsed(start,end):
+        return end-start
+
+    """Find a BlockType by Name via a string instance"""
+    def FindBlockTypeByName(self, BlockTypeName):
+        BlockTypeName = str(BlockTypeName)
+
+        if not isinstance(BlockTypeName, str):
+            assert "NifExplorer.FindBlockTypeByName(): Parameter 'BlockTypeName' Must be a string!"
+
+        for object in getattr(sys.modules["pyffi.formats.nif"], "NifFormat").__dict__.values():
+            if hasattr(object, "__name__"):
+                if object.__name__ == BlockTypeName and object != None:
+                    return object
+
+        return None
+
+    """Searches for a directory recursively for .nif files"""
+    @staticmethod
+    def DirectoryContainsNifRecursively(path):
+        for SubDirectrory, Directories, Files in os.walk(path):
+            for FileName in Files:
+                FilePath = SubDirectrory + os.sep + FileName
+
+                if FilePath.endswith(".nif"):
+                    return True
+
+        return False        
 
     """Returns a string derived from a NifFormat BlockType"""
     @staticmethod
     def BlockTypeToString(BlockType):
+        if BlockType == None:
+            assert "NifExplorer.BlockTypeToString(): BlockType shouldn't be None"
+            return
+
         s = str(BlockType)
         strings = s.split("'")
 
@@ -90,15 +149,3 @@ class NifExplorer():
             assert "NifExplorer.MakeAbsolutePath(): Cou;d not make absolute path!"
 
         return str
-
-    """Find a BlockType by Name via a string instance"""
-    def FindBlockTypeByName(self, BlockTypeName):
-        BlockTypeName = str(BlockTypeName)
-
-        if not isinstance(BlockTypeName, str):
-            assert "NifExplorer.FindBlockTypeByName(): Parameter 'BlockTypeName' Must be a string!"
-
-        for object in getattr(sys.modules["pyffi.formats.nif"], "NifFormat").__dict__.values():
-            if hasattr(object, "__name__"):
-                if object.__name__ == BlockTypeName and object != None:
-                    return object
